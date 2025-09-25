a8944009b8966cb6b07cf34a243277ad0439c936
theyorubayesian <akintunde.oladipo@taresco.org>
2025-09-25 13:27:02 +0000

    updated training script

--- scripts/train_lid.sh
+++ scripts/train_lid.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 function train(){
-    python -m agbaye.lid.train_fasttext_model train-model \
-    --train_dataset "data/lid_training_data/tiny.tsv" \
-    --model_dir "models" \
+    python -m agbaye.lid.training.train_fasttext train-model \
+    --train-dataset "/home/theyorubayesian/scratch/agbaye/data/all_glotlid_data.tsv" \
+    --validation-dataset-or-dir /store/shared/flores_plus/devtest \
+    --output-dir "/store/shared/wmqds/experiments/20250923224618/" \
+    --at-k 1 \
+    --at-k 3 \
+    --at-k 5 \
+    --at-k 10 \
     --threshold 1e-4 \
     --lr 0.1 \
     --lrupdaterate 100 \
@@ -17,14 +22,18 @@ function train(){
     --wordngrams 2 \
     --minn 3 \
     --maxn 6 \
-    --report_to_wandb
+    --threads 4
 }
 
 function evaluate() {
     python -m agbaye.lid.train_fasttext_model evaluate-model \
     --model_name_or_path "OpenLID" \
+    --output_dir \
     --eval_dataset "data/lid_training_data/lid_eval.tsv" \
-    --report_to_wandb
+    --report_to_wandb \
+    --wandb_entity taresco-hq \
+    --wandb_project wmqds-lid
 }
 
-evaluate
+# evaluate
+# train
